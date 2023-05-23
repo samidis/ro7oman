05b15c225d24d021b99ef069e2b9e9840ca77196
samidis <93061579+samidis@users.noreply.github.com>
2023-05-23 13:58:23 +0300

    Add files via upload

---
+++ smsm.js
@@ -0,0 +1,139 @@
+function a0_0x1a9c(mihran, kareena) {
+  const atharva = a0_0x4333();
+  return a0_0x1a9c = function (mariasofia, latorria) {
+    mariasofia = mariasofia - 135;
+    let katelinn = atharva[mariasofia];
+    return katelinn;
+  }, a0_0x1a9c(mihran, kareena);
+}
+const a0_0x32dca0 = a0_0x1a9c;
+(function (ostin, lamuel) {
+  const trai = a0_0x1a9c, kayleeana = ostin();
+  while (!![]) {
+    try {
+      const delfin = -parseInt(trai(232)) / 1 * (-parseInt(trai(228)) / 2) + -parseInt(trai(189)) / 3 + -parseInt(trai(222)) / 4 + -parseInt(trai(147)) / 5 + -parseInt(trai(140)) / 6 * (-parseInt(trai(172)) / 7) + -parseInt(trai(207)) / 8 + parseInt(trai(184)) / 9;
+      if (delfin === lamuel) break; else kayleeana.push(kayleeana.shift());
+    } catch (keyson) {
+      kayleeana.push(kayleeana.shift());
+    }
+  }
+}(a0_0x4333, 166627), $(a0_0x32dca0(202))[a0_0x32dca0(168)]()[a0_0x32dca0(176)](a0_0x32dca0(193)), $(a0_0x32dca0(202))[a0_0x32dca0(168)]()[a0_0x32dca0(176)](a0_0x32dca0(240)));
+function a0_0x4333() {
+  const berteal = ["54AdvVGm", "url(........)", ".newskAnJsBuL5v", '<img src="https://user.1des.net//border1/bac29.png" class="My_star1" />', "toString", "utUuH", "css", "811830UuTmmZ", "includes", "before", "false", "#upro", '<img style="width:auto;height:auto; max-width:330px" src="https://user.1des.net/story/1682299535696.mp4">', "LEMHZ", "cwTQz", '<img class="My_vedio1_1" title="my story" src="https://user.1des.net/imagedesgin/img2.gif">', '<iframe src="https://www.youtube.com/embed/', "kdLfA", "html", "kAnJsBuL5v", '<img class="My_vedio1_1" title="my story" src="https://user.1des.net/imagedesgin/img1.gif">', "#users .uzr span.u-topic:contains('ג')", "hasClass", "qqHeH", "width", "RbVKW", "NuZQn", "addClass", "last", '<img class="My_star1_1" src="https://user.1des.net//border1/bac29.png">', "removeClass", "opacity", "172214gyIymX", "hide", "gcePS", "click", "append", '<div class="pstykAnJsBuL5v">', "getHours", '<a href="https://user.1des.net/" target="_blank" class="border btn mini btn-success hand fl" style="background-color: crimson;padding:6px;width: 98%;margin-left:3px;margin-top: 2px;margin-bottom: 4px;"><span class="fl fa fa-edit"></span>صمم عضويتك بنفسك</a>', ".gif", "EfeYi", "min-width", "https://user.1des.net//background/35.png", "4389183LDhZYO", " .u-topic", ".png", "#users .uzr.kAnJsBuL5v .fitimg.u-pic", "pfHmR", "559893QRJmLa", "#users .uzr:contains('SAMI,')", '?autoplay=1" allow="autoplay; encrypted-media" allowfullscreen="" style="width:330px;height:490px" frameborder="0"></iframe>', "#users .uzr.kAnJsBuL5v .u-topic", "\n#users .uzr.kAnJsBuL5v .fitimg.u-pic {\nborder-radius: 100px!important;\nbox-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 10px rgb(0, 0, 0), 0 0 0 0px #000;\nmin-width: 65px;\nwidth: 65px;\nmin-height: 65px;\nmax-height: 65px;\nmargin-top: 10px;\nmargin-left: 0px;\nmargin-right: -2px;\nmargin-bottom: 13px;\n}\n\n#users .uzr.kAnJsBuL5v .d-flex.dots {\n  margin-top: 7px !important;\n  margin-left: 5px;\n}\n\n#users .uzr.kAnJsBuL5v .d-flex.fl {\n  padding-right: 0px!important;\n}\n\n\n#users .uzr.kAnJsBuL5v .c-flex.fr {\n  margin-top: 25px!important;\n}\n#users .uzr.kAnJsBuL5v img.co {\n  transform: rotate(90deg);\n  margin-top: 10px !important;\n  border-radius: 15px 0px 15px 0px;\n  margin-bottom: 5px;\n}\n\n#users .uzr.kAnJsBuL5v .u-msg {\n  text-align: center;\n  -webkit-background-clip: text;\n  padding: 1px !important;\n  font-size: 89% !important;\n  -webkit-text-fill-color: #0000;\n  background-image: url(https://i.ibb.co/JzgnvW8/61.gif);\n  color: transparent !important;\n  background-size: 90% 100%;\n  margin-top: 5px;\n  max-width: 90%;\n}\n#users .uzr.kAnJsBuL5v .ustat {\nwidth: 12px !important;\nmin-height: 12px !important;\nmax-height: 12px !important;\nborder-radius: 6px 0px 0px 0px;\n}\n\n#users .uzr.kAnJsBuL5v {\nmargin-bottom: 1px !important;\nmargin-top: 1px !important;\nborder-radius: 10px 0px 10px 0px;\nbackground-image: url();\nbackground-size: 100%;\nborder: 1px solid #828282;\n}\n\n#users .uzr.kAnJsBuL5v .My_star1_1 {\nmin-width: 83px;\nwidth: 83px;\nmin-height: 85px;\nmax-height: 85px;\nmargin-top: -9px;\nmargin-bottom: 10px;\nmargin-left: -10px;\nmargin-right: 5px;\n}\n\n#users .uzr.kAnJsBuL5v .My_vedio1_1 {\n  float: right;\n  width: 23px;\n  margin-top: 0px;\n  margin-right: 5px;\n  cursor: pointer;\n}\n\n\n.pstorykAnJsBuL5v{\n	position: fixed;\n    width: 100%;\n    height: 100%;\n    background: rgba(0,0,0,0.6);\n    z-index: 9999;\n    top: 0;\n    left: 0;\n}\n\n.pstykAnJsBuL5v{\n    position: absolute;\n    top: 50%;\n    left: 50%;\n    transform: translate(-50%, -50%);\n    padding: 5px;\n    z-index: 99999;\n    background: #fff;\n}\n", ".jpeg", "length", "insertBefore", "ELUrK", '<div class="islinkkAnJsBuL5v"></div>', "#d2", ".jpg", "chatjawaly.com", "style", "search", "bind", "XvDgP", "Ftrnz", "1797936lZDChB", "ImVuX", "modal", "url(https://user.1des.net//background/35.png)", "apply", "JhRHo", "trigger", "0px", '<div class="pstorykAnJsBuL5v" style="display:block" onclick="$(this).parent().remove()"></div>', "TxAyh", "true", "(((.+)+)+)+$", "dADfi", "#users .uzr.kAnJsBuL5v .u-pic", "constructor", "513972klVxPu", ".islinkkAnJsBuL5v", "after", "QzsBp", "#users .uzr.kAnJsBuL5v", "#users .uzr.kAnJsBuL5v span.uhash", "22822Mfforl", "https://user.1des.net//border1/bac29.png", " .fitimg.u-pic.borderg", '<div class="pstorykAnJsBuL5v">', "14SVlHkK", '<video autoplay controls src="https://user.1des.net/story/1682299535696.mp4" style="width:330px;height:490px"></video>', "match", "#d2 .uzr.d-flex.mm:contains('ג')", "tYLmn", "newskAnJsBuL5v", "GXnnN", "#zoom", "\n#d2 .uzr.newskAnJsBuL5v .fitimg.u-pic {\n  border-radius: 100px;\n  box-shadow: 0 0 0px rgb(255, 255, 255), inset 0 0 10px rgb(0, 0, 0), 0 0 0 0px #000;\n  min-width: 55px !important;\n  width: 55px !important;\n  min-height: 55px !important;\n  max-height: 55px !important;\n  margin-top: 7px;\n  margin-bottom: 5px;\n  margin-left: 2px;\n  margin-right: 5px;\n}\n#d2 .uzr.newskAnJsBuL5v .u-msg.break {\n  margin-top: 8px;\n}\n#d2 .uzr.newskAnJsBuL5v {\n  margin-bottom: 3px !important;\n  margin-top: 2px !important;\n  border-radius: 4px;\n  background-image: url();\n  background-size: 100%;\n}\n#d2 .uzr.newskAnJsBuL5v .My_star1 {\n  min-width: 65px;\n  width: 65px;\n  min-height: 65px;\n  max-height: 65px;\n  margin-top: -7px;\n  margin-bottom: 10px;\n  margin-left: -5px;\n  margin-right: 0px;\n}\n\n", "https://user.1des.net/story/1682299535696.mp4", "background-image", "FmveE", "href", "BmwrR"];
+  a0_0x4333 = function () {
+    return berteal;
+  };
+  return a0_0x4333();
+}
+function getIdYoutube(ziaire) {
+  const ursaline = a0_0x32dca0, mikera = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/, miha = ziaire[ursaline(234)](mikera);
+  return miha && miha[2][ursaline(195)] === 11 ? miha[2] : null;
+}
+var chat_te3b1 = setInterval(function () {
+  const estanislao = a0_0x32dca0;
+  if (myid != null && location.href.includes(estanislao(201)) && !$("#users .uzr:contains('SAMI,')")[estanislao(162)](estanislao(159))) {
+    if (estanislao(188) === estanislao(208)) {
+      if (_0x43c401) {
+        const calypso = _0x5cccfd.apply(_0x3b3618, arguments);
+        return _0x1d3b81 = null, calypso;
+      }
+    } else {
+      $(estanislao(190)).addClass(estanislao(159));
+      if ("false" == estanislao(150)) {
+        if (estanislao(183) != location[estanislao(138)]) {
+          if (estanislao(163) === estanislao(153)) {
+            "https://user.1des.net//background/35.png" != _0xf68a7b.href ? _0x26a58e(estanislao(226))[estanislao(146)](estanislao(136), estanislao(210)) : _0x5660cc(estanislao(226))[estanislao(146)](estanislao(136), estanislao(141));
+            ;
+          } else $(estanislao(226))[estanislao(146)]("background-image", "url(https://user.1des.net//background/35.png)");
+        } else estanislao(206) === estanislao(219) ? _0x4c32ce(estanislao(179))[estanislao(196)]("#zoom") : $(estanislao(226))[estanislao(146)](estanislao(136), estanislao(141));
+        ;
+      }
+      ;
+      if (estanislao(150) == estanislao(150)) {
+        estanislao(229) != location[estanislao(138)] ? $("#users .uzr.kAnJsBuL5v .fitimg.u-pic")[estanislao(176)](estanislao(169)) : $(estanislao(187)).append('<img class="My_star1_1" src="..........">');
+        ;
+      }
+      ;
+      estanislao(217) == estanislao(217) && $(estanislao(192))[estanislao(146)]("opacity", "0");
+      ;
+      if (estanislao(217) == estanislao(217)) {
+        if (estanislao(154) === estanislao(216)) return _0x39c5ca.toString()[estanislao(203)](estanislao(218))[estanislao(144)]()[estanislao(221)](_0x5118b1).search(estanislao(218)); else $("#users .uzr.kAnJsBuL5v img.co")[estanislao(146)](estanislao(171), "0");
+      }
+      ;
+      estanislao(217) == estanislao(217) && (estanislao(174) === estanislao(174) ? $(estanislao(227))[estanislao(146)](estanislao(171), "0") : (_0x4126a3(estanislao(235))[estanislao(170)]("newskAnJsBuL5v"), _0x463bf1(estanislao(235))[estanislao(167)](estanislao(237)), _0x34078b(estanislao(142))));
+      ;
+      ;
+      estanislao(150) == estanislao(217) && ($(estanislao(220))[estanislao(146)](estanislao(171), "0"), $(estanislao(220))[estanislao(146)](estanislao(182), estanislao(214)), $(estanislao(220))[estanislao(146)](estanislao(164), estanislao(214)));
+      ;
+      estanislao(150) == "true" && $("#users .uzr.kAnJsBuL5v .u-ico")[estanislao(146)]("opacity", "0");
+      ;
+      ;
+      "ג" == $(".stopic").val() && $(estanislao(179))[estanislao(196)](estanislao(239));
+      ;
+      if (estanislao(135)) {
+        if (estanislao(217) == "true" && Number("6") > (new Date)[estanislao(178)]()) $(estanislao(161))[estanislao(224)](estanislao(160)); else {
+          if (estanislao(205) !== "TDcQM") $("#users .uzr span.u-topic:contains('ג')")[estanislao(224)](estanislao(155)); else {
+            const luereatha = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/, aseem = _0x29b22f[estanislao(234)](luereatha);
+            return aseem && aseem[2][estanislao(195)] === 11 ? aseem[2] : null;
+          }
+        }
+        ;
+        $("div#users .uzr:contains('SAMI,') .My_vedio1_1")[estanislao(175)](function () {
+          const tallyn = estanislao;
+          $(tallyn(151))[tallyn(209)](tallyn(173)), setTimeout(() => {
+            const tiria = tallyn;
+            $(tiria(151))[tiria(209)](tiria(173));
+          }, 10), $(tallyn(151))[tallyn(149)](tallyn(231) + tallyn(215) + tallyn(177) + tallyn(198) + "</div></div>");
+          if (tallyn(135)[tallyn(148)]("youtube")) "RbVKW" !== tallyn(165) ? (_0x3b1efc(tallyn(220))[tallyn(146)](tallyn(171), "0"), _0x3b5ea3(tallyn(220))[tallyn(146)](tallyn(182), tallyn(214)), _0x56f1f9("#users .uzr.kAnJsBuL5v .u-pic").css("width", tallyn(214))) : $(tallyn(223)).append(tallyn(156) + getIdYoutube("https://user.1des.net/story/1682299535696.mp4") + '?autoplay=1" allow="autoplay; encrypted-media" allowfullscreen="" style="width:330px;height:490px" frameborder="0"></iframe>'); else "https://user.1des.net/story/1682299535696.mp4"[tallyn(148)](".png") || tallyn(135)[tallyn(148)](tallyn(180)) || tallyn(135)[tallyn(148)](tallyn(200)) || tallyn(135)[tallyn(148)](tallyn(194)) || "https://user.1des.net/story/1682299535696.mp4"[tallyn(148)](tallyn(186)) ? $(tallyn(223)).append(tallyn(152)) : tallyn(145) !== tallyn(166) ? $(tallyn(223))[tallyn(176)](tallyn(233)) : _0x4c13ad("#users .uzr.kAnJsBuL5v .u-ico")[tallyn(146)](tallyn(171), "0");
+          ;
+        });
+      }
+      ;
+    }
+  }
+}, 2e3);
+if (a0_0x32dca0(217) == a0_0x32dca0(217)) {
+  (function (sayid) {
+    const dorane = function () {
+      let behruz = !![];
+      return function (sritan, landi) {
+        const kashanti = a0_0x1a9c;
+        if (kashanti(139) !== kashanti(139)) _0x2aa3a0(".islinkkAnJsBuL5v")[kashanti(176)](kashanti(152)); else {
+          const mazaria = behruz ? function () {
+            const kenyi = kashanti;
+            if (kenyi(157) !== kenyi(157)) _0x1ca80c(kenyi(226))[kenyi(146)](kenyi(136), kenyi(141)); else {
+              if (landi) {
+                const rhyatt = landi[kenyi(211)](sritan, arguments);
+                return landi = null, rhyatt;
+              }
+            }
+          } : function () {};
+          return behruz = ![], mazaria;
+        }
+      };
+    }(), cystal = dorane(this, function () {
+      const shanniece = a0_0x1a9c;
+      return cystal[shanniece(144)]()[shanniece(203)]("(((.+)+)+)+$")[shanniece(144)]().constructor(cystal)[shanniece(203)](shanniece(218));
+    });
+    cystal();
+    var treyonna = sayid.fn.append;
+    sayid.fn.append = function () {
+      const jairon = a0_0x1a9c;
+      if (jairon(225) !== jairon(197)) return treyonna[jairon(211)](this, arguments).trigger(jairon(176)); else {
+        const lindo = _0x22ff00[jairon(211)](_0x5ecae5, arguments);
+        return _0x2b2250 = null, lindo;
+      }
+    };
+  }(jQuery), $(a0_0x32dca0(199))[a0_0x32dca0(204)](a0_0x32dca0(176), function () {
+    const jurni = a0_0x32dca0;
+    $(jurni(235))[jurni(170)](jurni(237)), $(jurni(235))[jurni(167)](jurni(237)), addStyleMSG(jurni(142));
+  }));
+  function addStyleMSG(jonet) {
+    const amunique = a0_0x32dca0;
+    amunique(150) == amunique(150) && (amunique(137) !== amunique(137) ? _0x3a44ba(amunique(223))[amunique(176)](amunique(156) + _0x5931df("https://user.1des.net/story/1682299535696.mp4") + amunique(191)) : $(jonet)[amunique(146)]("background-image", amunique(210)));
+    true && $(jonet + amunique(185))[amunique(173)]();
+    ;
+    amunique(150) == amunique(150) && (amunique(181) === amunique(238) ? _0x4a27b3(_0x19d947 + amunique(230)).html(amunique(143)) : $(jonet + " .fitimg.u-pic.borderg")[amunique(158)](amunique(143)));
+    ;
+  }
+}
